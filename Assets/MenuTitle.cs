13d4b8553f527d2813e8b09961a3653d08fd2d6c
DavidOliveiraSilva <clear844@gmail.com>
2018-04-12 21:42:31 -0300

    Sons do jogo e Menu

--- Assets/MenuTitle.cs
+++ Assets/MenuTitle.cs
@@ -1,16 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuTitle : MonoBehaviour {
-
+	public string firstSceneName;
+	private AudioManager audioManager;
+	private bool play = false;
+	private GameObject blackScreen;
+	private float time = 0;
 	// Use this for initialization
 	void Start () {
-		
+		audioManager = AudioManager.instance;
+		blackScreen = GameObject.Find ("Black Screen");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (play) {
+			time += Time.deltaTime;
+			if (time >= blackScreen.GetComponent<GradientColorChanger> ().endChange) {
+				SceneManager.LoadScene (firstSceneName);
+			}
+			return;
+		}
+		if (Input.anyKeyDown) {
+			audioManager.PlaySound ("Play");
+			blackScreen.GetComponent<GradientColorChanger> ().enabled = true;
+			play = true;
+		}
+
 	}
 }
